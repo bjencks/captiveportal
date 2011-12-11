373a9bf9adbfffc7a54a42021301a28cee691224
Ben Jencks <ben@bjencks.net>
2011-12-11 15:13:17 -0500

    Add README and license for public release

--- lib/mac.py
+++ lib/mac.py
@@ -1,5 +1,27 @@
+# Copyright 2011 Ben Jencks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 class MAC:
+    """Simple container for a MAC address, with parsing and formatting routines.
+
+    """
     def __init__(self, inp):
+        """Construct a MAC from either a bytes object (6 bytes) or a hex string
+        in standard, Cisco, or non-delimited format.
+        
+        """
         if isinstance(inp, bytes):
             if len(inp) == 6:
                 self.bytes = inp
@@ -8,8 +30,13 @@ class MAC:
         elif isinstance(inp, str):
             self.bytes = _mac_str_to_bytes(inp)
     def rawstr(self):
+        """Return a non-delimited hex representation of the address"""
         return ''.join(['{0:02x}'.format(x) for x in self.bytes])
     def __str__(self):
+        """Return a representation of the address formatted in standard
+        colon-delimited form.
+        
+        """
         return ':'.join(['{0:02x}'.format(x) for x in self.bytes])
     def __repr__(self):
         return 'MAC(\'{0}\')'.format(str(self))
